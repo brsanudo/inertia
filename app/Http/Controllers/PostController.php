d7eb77aee730e1ed9ce5149228ab5fafd9fdc579
bryan <bryandofus3229@gmail.com>
2023-01-19 15:14:05 -0500

    deploy

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render("Posts/Index",[
+            "posts" => Post::with("user:id,name")->latest()->get()
+        ]);
+    }
+
+   public function store(Request $request)
+    {
+        
+        $validated = $request->validate([
+            "title" => "required|string|max:100",
+            "body" => "required|string|max:255"
+        ]);
+
+        $request->user()->posts()->create($validated);
+        return redirect(route("posts.index"));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $this->authorize("update",$post);
+        $validated = $request->validate([
+            "title" => "required|string|max:100",
+            "body" => "required|string|max:255"
+        ]);
+
+        $post->update($validated);
+        return redirect(route("posts.index"));
+    }
+
+    
+   
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
